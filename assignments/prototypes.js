52d88f73dcb1859cf6a8b04733e439f5d5498f2f
Myke47 <mychel.larson@gmail.com>
2019-04-01 23:40:49 -0700

    staging small updates and models of 'this' principles

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,14 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+const game = new GameObject({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  }
+})
 
 /*
   === CharacterStats ===
@@ -41,7 +49,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +110,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,77 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Binding or Window Binding
+* 2. Implicit Binding
+* 3. New Binding
+* 4. Explicit Binding 
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
+// console.log(this);
+
 // code example for Window Binding
 
 // Principle 2
+
+const myObj = {
+      greeting: 'Hello',
+      speak: function() {
+        console.log('implicit this: ===', this);
+        return '${this.greeting}, world!'
+      }
+    };
+    myObj.speak();
 
 // code example for Implicit Binding
 
 // Principle 3
 
+function CordialPerson(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting + this.greeter);
+      console.log(this);
+    };
+  }
+  
+  const jerry = new CordialPerson('Newman');
+  const newman = new CordialPerson('Jerry');
+  
+  newman.speak();
+  newman.speak();
+
 // code example for New Binding
 
 // Principle 4
+
+const myObj2 = {
+    greeting: 'Hello',
+    speak: function() {
+      console.log('implicit this: ===', this);
+      return '${this.greeting}, world!'
+    }
+  };
+  
+  myObj2.speak();
+  
+  function Person(obj)  {
+    this.name = obj.name;
+    this.age = obj.age;
+    this.speak = function() {
+      console.log('This new binding' ,this);
+      return 'Hello, my name is ${this.name}, and I am ${this.age} years old!';
+    }
+  }
+  
+  const myke = new Person({name: 'Myke', age: 40});
+  const nevan = new Person({name: 'Nevan', age: '13'});
+  
+  nevan.speak.call(myke); 
+  myke.speak.apply(nevan);
 
 // code example for Explicit Binding
